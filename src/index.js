cbc13623fed91db707c54940cbb4b7d14f3dd9a5
Алексей Данилов <mr.javez@ukr.net>
2024-04-28 21:30:57 +0300

    update main app index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const router = require("./routes/main.routes");
-const app = express();
+const mainRoutes = require("./routes/main.routes");
+const authRoutes = require("./routes/auth.routes");
+const actionRoutes = require("./routes/action.routes");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const env = process.env.NODE_ENV || "development";
+const app = express();
+
+app.use("/", mainRoutes)
+app.use("/auth", authRoutes);
+app.use("/actions", actionRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 const PORT = process.env[`${env.toUpperCase()}_APP_PORT`] || "";
-
-app.use(router)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
